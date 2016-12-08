9ab3f3c63c56ee8c1e6ec1eba08ad683e83c0217
Esteban Zapata <jezg1992@hotmail.com>
2016-12-08 15:48:23 -0500

    OracleCourse:
    First practice - Oracle course Java Lambda

---
+++ src/oraclecourse/OracleCourse.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package oraclecourse;
+
+import lesson1.Lesson1;
+
+/**
+ *
+ * @author PC
+ */
+public class OracleCourse {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Lesson1 lesson = new Lesson1();
+        lesson.runExercises();
+    }
+
+}
